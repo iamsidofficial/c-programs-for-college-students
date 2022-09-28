c18912a1f9dd69db0d8d21d1feae4ebc0f3aa50d
Harjaspal Singh <71326879+Harjas-Sidhu@users.noreply.github.com>
2022-09-28 21:30:50 +0530

    Update matrix_multiplication.c

--- matrix_multiplication.c
+++ matrix_multiplication.c
@@ -1,62 +1,67 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+int res[20][20];
+
+void val(int mat[20][20], int r, int c, int n)
+{
+    for (int i = 1; i <= r; i++)
+    {
+        for (int j = 1; j <= c; j++)
+        {
+            printf("Enter the Value of mat%d[%d][%d]\n", n, i, j);
+            scanf("%d", &mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void mul(int mat1[20][20], int mat2[20][20], int r1, int c2, int r2)
+{
+    for (int i = 1; i <= r1; i++)
+    {
+        for (int j = 1; j <= c2; j++)
+        {
+            for (int n = 1; n <= r2; n++)    
+
+            {
+                res[i][j] += mat1[i][n] * mat2[n][j];
+            }
+        }
+    }
+}
+
 int main()
 {
-    int m, n, m2, n2, sum = 0;
-    printf("No of coloum of first matrix must be equal to the no. of rows in second matrix\n ");
-    printf("Enter the number of rows and coloum of your first matrix\n");
-    scanf("%d %d", &m, &n);
-    printf("Enter the number of rows and coloum of your first matrix\n");
-    scanf("%d %d", &m2, &n2);
-    if (n == m2)
+    int r1, r2, c1, c2;
+    int mat1[20][20], mat2[20][20];
+    printf("\n*****     For Matrix 1     *****\n");
+    printf("Enter the number of Rows:\n");
+    scanf("%d", &r1);
+    printf("Enter the number of Columns:\n");
+    scanf("%d", &c1);
+    val(mat1, r1, c1, 1);
+    printf("\n\n*****     For Matrix 2     *****\n");
+    printf("Enter the number of Rows:\n");
+    scanf("%d", &r2);
+    printf("Enter the number of Columns:\n");
+    scanf("%d", &c2);
+    if (c1 == r2)
     {
-        int a[m][n], b[m2][n2], result[m][n2];
-        printf("Enter your First matrix\n");
-
-        for (int i = 0; i < m; i++)
+        val(mat2, r2, c2, 2);
+        mul(mat1, mat2, r1, c2, r2);
+        for (int i = 1; i <= r1; i++)
         {
-            for (int j = 0; j < n; j++)
+            for (int j = 1; j <= c2; j++)
             {
-                scanf("%d", &a[i][j]);
+         
+       printf("Value of res[%d][%d] is %d\n", i, j, res[i][j]);
             }
-        }
-
-        printf("Enter your second matrix\n");
-        for (int i = 0; i < m2; i++)
-        {
-            for (int j = 0; j < n2; j++)
-            {
-                scanf("%d", &b[i][j]);
-            }
-        }
-
-        printf("Resultant matrix is\n");
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n2; j++)
-            {
-                for (int k = 0; k < n; k++)
-                {
-                    sum += a[i][k] * b[k][j];
-                }
-                result[i][j] = sum;
-                sum = 0;
-            }
-        }
-
-        for (int i = 0; i < m; i++)
-        {
-            for (int j = 0; j < n2; j++)
-            {
-
-                printf("%d \t", result[i][j]);
-            }
-            printf("\n");
         }
     }
     else
     {
-        printf("Multiplication of this matrix is not possible");
+        printf("Multiplication is not Possible.\n");
     }
+    
     return 0;
 }
