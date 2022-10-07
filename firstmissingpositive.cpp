0016e88a93959221d7dd91b2bebdadf301bc5983
yuvihere23 <114753071+yuvihere23@users.noreply.github.com>
2022-10-07 14:04:18 +0530

    added first missing positive number problem in cpp language

---
+++ firstmissingpositive.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    int firstMissingPositive(int A[], int n)
+    {
+        for(int i = 0; i < n; ++ i)
+            while(A[i] > 0 && A[i] <= n && A[A[i] - 1] != A[i])
+                swap(A[i], A[A[i] - 1]);
+        
+        for(int i = 0; i < n; ++ i)
+            if(A[i] != i + 1)
+                return i + 1;
+        
+        return n + 1;
+    }
+};
