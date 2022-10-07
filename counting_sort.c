e07ecdef8bff512746501848737beb8d5f91f6e6
Tarun <104731395+Tarun-pvc@users.noreply.github.com>
2022-10-07 19:54:42 +0530

    Update counting_sort.c
    Fixed the segfault.

--- counting_sort.c
+++ counting_sort.c
@@ -8,10 +8,11 @@ void counting_sort(int array[], int size)
 {
     int max = max_element(array, size);
 
-    int temp[max];          // a temporary array which will let us find and store the ranks of each element in the main array.
+    int temp_size = max + 1;
+    int temp[temp_size];    // a temporary array which will let us find and store the ranks of each element in the main array.
     int sorted_array[size]; // array to be returned with sorted elements
 
-    for (int i = 0; i < max; i++)
+    for (int i = 0; i < temp_size; i++)
     {
         temp[i] = 0;
     }
@@ -22,7 +23,7 @@ void counting_sort(int array[], int size)
         temp[array[i]]++;
     }
 
-    for (int i = 1; i < max; i++)
+    for (int i = 1; i < temp_size; i++)
     {
         // finding and storing the rank of each element (number of elements lower than each element)
         temp[i] = temp[i] + temp[i - 1];
@@ -42,7 +43,7 @@ void counting_sort(int array[], int size)
     // the sorted array can directly be returned here if the original array shouldnt be changed
 }
 
-// utility functions
+// utility function to print an array
 
 int max_element(int array[], int size)
 {
@@ -65,15 +66,14 @@ void print_array(int array[], int size)
     }
 }
 
-// main function
 int main()
 {
     // example
-    int array[] = {5, 1, 2, 7, 12, 11, 8};
-    // this sort is not feasible if the greatest element in the array is very big.
-    // the greatest element in the array should stay low for direct counting sort to work properly without any memory issues.
-    
+    int array[] = {5, 13, 2, 7, 12, 11, 8};
+    // this sort is not effective at all if the greatest element in the array is very big.
+    // if you are editing this, make sure the greatest element stays low.
     int size = sizeof(array) / sizeof(array[0]);
+
     counting_sort(array, size);
 
     print_array(array, size);
