d1c53a7d94ad881d25cc3c560da9690cd9357ca8
Zeeshan <66550180+ZeeshanK07@users.noreply.github.com>
2022-10-10 18:28:50 +0530

    Added option to check another number
    Added option to check another number without restarting or re-compiling the programme so that the user did not face issues of multiple restarts also added the option at the starting so that the user can see which number has been selected.

--- negative positive.c
+++ negative positive.c
@@ -5,14 +5,28 @@
  
 void main()
 {
-    int num;
- 
+    int num,attempt;
+
+    start:
     printf("Enter a number: \n");
     scanf("%d", &num);
+    
+    printf("You have entered %d number to check whether the number is positive or negative\n",num);
+    
     if (num > 0)
         printf("%d is a positive number \n", num);
     else if (num < 0)
         printf("%d is a negative number \n", num);
     else
         printf("0 is neither positive nor negative");
+
+    printf("Press 1 if you want to check another number\n");
+    scanf("%d",&attempt);
+
+    if (attempt==1)
+    {
+        goto start;
+    }
+    else{printf("Thank you for using this programme!!!");}
+
 }
