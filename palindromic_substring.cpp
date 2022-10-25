637d2c9b92ddc61f1fec774911605b3f825ee2d7
kaushalsingh584 <64363016+kaushalsingh584@users.noreply.github.com>
2022-10-25 16:09:04 +0530

    created palindromic_substring.cpp
    added the solution to the problem palindromic substring

---
+++ palindromic_substring.cpp
@@ -0,0 +1,29 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+    int main(){
+        string s;
+        cout<<"Enter the string you want to check";
+        cin>>s;
+        int n=s.size();
+        int ans=0;
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        
+        for(int gap=0;gap<n;gap++){
+            for(int i=0,j=gap;j<n;i++,j++){
+                if(gap==0){
+                    dp[i][j]=1;
+                    ans++;
+                }
+                else if(gap == 1 and s[i]==s[j]){
+                    dp[i][j]=1;
+                    ans++;
+                }else if(s[i] == s[j] and dp[i+1][j-1] ==1){
+                    dp[i][j]=1;
+                    ans++;
+                }
+            }
+        }
+        cout<< ans;
+    }
