9ebc49eeff3113add695cefe56d0156377a98f58
kaushalsingh584 <64363016+kaushalsingh584@users.noreply.github.com>
2022-10-25 19:20:04 +0530

    added longest_substring_without_repeating_charaters.cpp
    solution for the problem longest_substring_without_repeating_charaters

---
+++ longest_substring_without_repeating_charaters.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+    using namespace std;
+    
+    int main(){
+        
+    string s;
+    cin>>s;
+    
+    
+    unordered_map<char,int> mpp;
+    int i=0;
+    int j=0;
+    int mx = INT_MIN;
+        if(s.length() == 0){
+            return 0;
+        }
+    while(j<s.length()){
+        mpp[s[j]]++;
+        if(mpp.size() == j-i+1){
+            mx = max(mx, j-i+1);
+            j++;
+        }
+        else if(mpp.size() < j-i+1){ 
+            while(mpp.size() < j-i+1){
+                mpp[s[i]]--;
+                if(mpp[s[i]] == 0){
+                   mpp.erase(s[i]);
+                }
+                i++;
+            }
+            j++;
+        }
+    }
+   cout<< mx;
+    }
