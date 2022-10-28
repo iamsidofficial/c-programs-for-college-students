9df50cf6b399a810e4789609fb5b4eb82083f08b
sandeepsuman10315 <85451910+sandeepsuman10315@users.noreply.github.com>
2022-10-28 18:20:46 +0530

    Infix to postfix
    Infix to postfix using Stack

---
+++ Stack_Infix_to_Postfix.c
@@ -0,0 +1,67 @@
+//
+// Created by Sumit Suman on 29-09-2021.
+//
+
+#include<stdio.h>
+#include<ctype.h>
+#include <stdlib.h>
+
+char stack[10];
+int top = -1;
+
+void push(char x)
+{
+    stack[++top] = x;
+}
+
+char pop()
+{
+    if(top == -1)
+        return -1;
+    else
+        return stack[top--];
+}
+
+int priority(char x)
+{
+    if(x == '(')
+        return 0;
+    if(x == '+' || x == '-')
+        return 1;
+    if(x == '*' || x == '/')
+        return 2;
+    return 0;
+}
+
+int main()
+{
+    char expression[100];
+    char *e, x;
+    printf("Enter the expression : ");
+    scanf("%s",expression);
+    printf("\nPostfix: ");
+    e = expression;
+
+    while(*e != '\0')
+    {
+        if(isalnum(*e))
+            printf("%c ",*e);
+        else if(*e == '(')
+            push(*e);
+        else if(*e == ')')
+        {
+            while((x = pop()) != '(')
+                printf("%c ", x);
+        }
+        else
+        {
+            while(priority(stack[top]) >= priority(*e))
+                printf("%c ",pop());
+            push(*e);
+        }
+        e++;
+    }
+
+
+    return 0;
+}
